d2b36d6820264440156e03cec594328d77aef788
Taity-mini <1504693@rgu.ac.uk>
2017-03-08 15:00:35 +0000

    Gallery photo upload function *Basic file upload for images *Still need to make folder structure for images

--- models/gallery_photos.php
+++ models/gallery_photos.php
@@ -5,6 +5,7 @@
  * Date: 07/03/2017
  * Time: 18:46
  * Gallery Photo model class
+ *
 */
 
 class gallery_photos
@@ -324,5 +325,62 @@ class gallery_photos
         }
     }
 
+    //TODO: Upload photos - update to create folder structure
+
+
+    public function uploadPhoto()
+    {
+        $target_dir = "../uploads/photos/";
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+        // Check if image file is a actual image or fake image
+        if (isset($_POST["submit"])) {
+            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+            if ($check !== false) {
+                echo "File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            } else {
+                echo "File is not an image.";
+                $uploadOk = 0;
+            }
+        }
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            echo "Sorry, file already exists.";
+            $uploadOk = 0;
+            return false;
+        }
+        // Check file size
+        if ($_FILES["fileToUpload"]["size"] > 500000000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+            return false;
+        }
+        // Allow certain file formats
+        if ($imageFileType != "jpg" && $imageFileType != "JPG"  && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif"
+        ) {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+            return false;
+        }
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+            return false;
+            // if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                $this->setFilePath($target_file);
+                return true;
+
+                echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+                return false;
+            }
+        }
+    }
 
 }
