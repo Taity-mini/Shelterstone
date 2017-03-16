8f47520c3936d4790b0adad21e192c7748cd1321
Taity-mini <1504693@rgu.ac.uk>
2017-03-16 16:38:04 +0000

    Competition model class *Standard functions *Getters/setters *CRUD functions *List competitions function

--- models/competitions.php
+++ models/competitions.php
@@ -12,8 +12,7 @@ class competitions
 {
     //Private variables/Properties
 
-    private $compID, $authorID, $locationID, $title, $description, $date, $modified,
-       $userID, $category, $position, $totalScore;
+    private $compID, $authorID, $locationID, $title, $description, $date, $modified;
 
     function _constructor($compID = -1)
     {
@@ -53,28 +52,194 @@ class competitions
         return $this->date;
     }
 
-    //Competition results
-
-
-    public function getUserID()
+    public function getModifiedDate()
     {
-        return $this->userID;
+        return $this->modified;
     }
 
-    public function getCategory()
+    //Setters
+
+    public function setCompID($compID)
     {
-        return $this->category;
+        $this->compID = htmlentities($compID);
     }
 
-    public function getPosition()
+    public function setAuthorID($authorID)
     {
-        return $this->position;
+        $this->authorID = htmlentities($authorID);
     }
 
-    public function getTotalScore()
+    public function setLocationID($locationID)
     {
-        return $this->totalScore;
+        $this->locationID = htmlentities($locationID);
     }
+
+    public function setTitle($title)
+    {
+        $this->title = htmlentities($title);
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = htmlentities($description);
+    }
+
+    public function setDate($date)
+    {
+        $this->date = htmlentities($date);
+    }
+
+    public function setModified($modified)
+    {
+        $this->modified = htmlentities($modified);
+    }
+
+
+    //CRUD Methods
+
+    public function getAllDetails($conn)
+    {
+        $sql = "SELECT * from competitions WHERE compID = :compID";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':compID', $this->getCompID(), PDO::PARAM_STR);
+
+
+        //Get user's details from users table row
+        try {
+            $stmt->execute();
+            $results = $stmt->fetchAll();
+
+            foreach ($results as $row) {
+               $this->setAuthorID($row['userID']);
+               $this->setLocationID($row['locationID']);
+               $this->setTitle($row['title']);
+               $this->setDescription($row['description']);
+               $this->setDate($row['date']);
+               $this->setModified($row['modified']);
+            }
+
+        } catch (PDOException $e) {
+            return "Query Failed:" . $e->getMessage();
+        }
+    }
+
+    public function create($conn)
+    {
+        try {
+            //Save current date time to variable for insertion
+            $date = date('Y-m-d H:i:s');
+
+            //SQL Statement
+            $sql = "INSERT INTO competitions VALUES (null, :userID, :locationID ,:title, :description, :date, :modified)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':userID', $this->getAuthorID(), PDO::PARAM_STR);
+            $stmt->bindParam(':locationID', $this->getLocationID(), PDO::PARAM_STR);
+            $stmt->bindParam(':title', $this->getTitle(), PDO::PARAM_STR);
+            $stmt->bindParam(':description', $this->getDescription(), PDO::PARAM_STR);
+            $stmt->bindParam(':date', $date, PDO::PARAM_STR);
+            $stmt->bindParam(':modified', $date, PDO::PARAM_STR);
+            return true;
+
+        } catch (PDOException $e) {
+            dbClose($conn);
+            return "Create competition failed:" . $e->getMessage();
+        }
+    }
+
+    public function update($conn)
+    {
+        try {
+            $sql = "UPDATE competitions SET userID = :userID, title = :title, description = :description, date = :date, modified= :modified
+                    WHERE compID = :compID";
+
+            $date = date('Y-m-d H:i:s', strtotime(str_replace('-', '/', $this->getDate())));
+            $modified =date('Y-m-d H:i:s');
+            $stmt = $conn->prepare($sql);
+
+            $stmt->bindParam(':compID', $this->getCompID(), PDO::PARAM_STR);
+            $stmt->bindParam(':userID', $this->getUserID(), PDO::PARAM_STR);
+            $stmt->bindParam(':locationID', $this->getLocationID(), PDO::PARAM_STR);
+            $stmt->bindValue(':title', $this->getTitle(), PDO::PARAM_STR);
+            $stmt->bindValue(':description', $this->getDescription(), PDO::PARAM_STR);
+            $stmt->bindValue(':date', $date, PDO::PARAM_STR);
+            $stmt->bindValue(':modified', $modified, PDO::PARAM_STR);
+
+            $stmt->execute();
+            return true;
+        } catch (PDOException $e) {
+            dbClose($conn);
+            return "Update climbing logbook failed: " . $e->getMessage();
+        }
+    }
+
+    public function delete($conn, $userID)
+    {
+        $sql = "DELETE FROM competitions";
+
+        if (!is_null($userID)) {
+            $sql .= " WHERE userID = :userID";
+        }
+
+        if (is_null($userID)) {
+            $sql .= " WHERE compID = :compID";
+        }
+
+        $stmt = $conn->prepare($sql);
+
+        if (!is_null($userID)) {
+            $stmt->bindParam(':userID', $userID, PDO::PARAM_STR);
+        }
+        if (is_null($userID)) {
+            $stmt->bindParam(':compID', $this->getCompID(), PDO::PARAM_STR);
+        }
+
+        try {
+            $stmt->execute();
+            return true;
+        } catch (PDOException $e) {
+            return "Delete competition entry failed:" . $e->getMessage();
+        }
+    }
+
+
+    public function getTotalCount($conn)
+    {
+        $sql = "SELECT COUNT(*) FROM competitions";
+        $stmt = $conn->prepare($sql);
+
+        try {
+            $stmt->execute();
+            $results = $stmt->fetch();
+            $count = $results[0];
+            return $count;
+        } catch (PDOException $e) {
+            return "Database count comps query failed: " . $e->getMessage();
+        }
+    }
+
+
+    public function listAllComps($conn, $userID = null)
+    {
+        $sql = "SELECT * FROM competitions c";
+        if (!is_null($userID)) {
+            $sql .= " WHERE userID = :userID";
+        }
+
+        $stmt = $conn->prepare($sql);
+
+        if (!is_null($userID)) {
+            $stmt->bindParam(':userID', $userID, PDO::PARAM_STR);
+        }
+
+        try {
+            $stmt->execute();
+            $results = $stmt->fetchAll();
+            return $results;
+        } catch (PDOException $e) {
+            return "List all competitions query failed: " . $e->getMessage();
+        }
+    }
+
 
 
 }
