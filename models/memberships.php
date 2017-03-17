6d52665353fa0b7a402e27fc12e857aded065c36
Taity-mini <1504693@rgu.ac.uk>
2017-03-17 00:18:04 +0000

    Memberships model class *Standard functions *Getters/setters *CRUD functions *List memberships results function *Input validation functions

--- models/memberships.php
+++ models/memberships.php
@@ -1,12 +1,284 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Andrew Tait (1504693)
  * Date: 14/03/2017
  * Time: 23:08
+ *
+ * Membership Model/Object class
  */
-
 class memberships
 {
+
+    //Properties/Variables
+    private $memberShipID, $userID, $type, $paid, $startDate, $endDate;
+
+    //Constructor
+    function _constructor($memberShipID = -1)
+    {
+        $this->memberShipID = htmlentities($memberShipID);
+    }
+
+    //Getters
+    public function getMemberShipID()
+    {
+        return $this->memberShipID;
+    }
+
+    public function getUserID()
+    {
+        return $this->userID;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function getPaid()
+    {
+        return $this->paid;
+    }
+
+    public function getStartDate()
+    {
+        return $this->startDate;
+    }
+
+    public function getEndDate()
+    {
+        return $this->endDate;
+    }
+
+    //Setters
+    public function setMemberShipID($memberShipID)
+    {
+        $this->memberShipID = htmlentities($memberShipID);
+    }
+
+    public function setUserID($userID)
+    {
+        $this->userID = htmlentities($userID);
+    }
+
+    public function setType($type)
+    {
+        $this->type = htmlentities($type);
+    }
+
+    public function setPaid($paid)
+    {
+        $this->paid = htmlentities($paid);
+    }
+
+    public function setStartDate($startDate)
+    {
+        $this->startDate = htmlentities($startDate);
+    }
+
+    public function setEndDate($endDate)
+    {
+        $this->endDate = htmlentities($endDate);
+    }
+
+    //CRUD methods
+
+    public function getAllDetails($conn)
+    {
+        $sql = "SELECT * from memberships WHERE memberShipID = :memberShipID";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':memberShipID', $this->getMemberShipID(), PDO::PARAM_STR);
+
+
+        //Get membership details from memberships table row
+        try {
+            $stmt->execute();
+            $results = $stmt->fetchAll();
+
+            foreach ($results as $row) {
+                $this->setUserID($row['userID']);
+                $this->type($row['type']);
+                $this->paid($row['paid']);
+                $this->setStartDate($row['startDate']);
+                $this->setEndDate($row['endDate']);
+            }
+
+        } catch (PDOException $e) {
+            return "Membership get details query Failed:" . $e->getMessage();
+        }
+    }
+
+    public function create($conn)
+    {
+        try {
+            //Save current date time to variable for insertion
+            $startDate = date('Y-m-d H:i:s', strtotime(str_replace('-', '/', $this->getStartDate())));
+            $endDate = date('Y-m-d H:i:s', strtotime(str_replace('-', '/', $this->getEndDate())));
+
+
+            //SQL Statement
+            $sql = "INSERT INTO memberships VALUES (null, :userID, :type ,:paid, :startDate, :endDate)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':userID', $this->getUserID(), PDO::PARAM_STR);
+            $stmt->bindParam(':type', $this->getType(), PDO::PARAM_STR);
+            $stmt->bindParam(':paid', $this->getPaid(), PDO::PARAM_STR);
+            $stmt->bindValue(':startDate', $startDate, PDO::PARAM_STR);
+            $stmt->bindValue(':endDate', $endDate, PDO::PARAM_STR);
+            return true;
+
+        } catch (PDOException $e) {
+            dbClose($conn);
+            return "Create membership failed:" . $e->getMessage();
+        }
+    }
+
+    public function update($conn)
+    {
+        try {
+            $sql = "UPDATE memberships SET userID = :userID, type = :type, paid = :paid, startDate = :startDate, endDate= :endDate
+                    WHERE memberShipID = :memberShipID";
+
+            $startDate = date('Y-m-d H:i:s', strtotime(str_replace('-', '/', $this->getStartDate())));
+            $endDate = date('Y-m-d H:i:s', strtotime(str_replace('-', '/', $this->getEndDate())));
+
+            $stmt = $conn->prepare($sql);
+
+            $stmt->bindParam(':memberShipID', $this->getMemberShipID(), PDO::PARAM_STR);
+            $stmt->bindParam(':userID', $this->getUserID(), PDO::PARAM_STR);
+            $stmt->bindParam(':type', $this->getType(), PDO::PARAM_STR);
+            $stmt->bindParam(':paid', $this->getPaid(), PDO::PARAM_STR);
+            $stmt->bindValue(':startDate', $startDate, PDO::PARAM_STR);
+            $stmt->bindValue(':endDate', $endDate, PDO::PARAM_STR);
+
+            $stmt->execute();
+            return true;
+        } catch (PDOException $e) {
+            dbClose($conn);
+            return "Update membership entry failed: " . $e->getMessage();
+        }
+    }
+
+    public function delete($conn, $userID)
+    {
+        $sql = "DELETE FROM memberships";
+
+        if (!is_null($userID)) {
+            $sql .= " WHERE userID = :userID";
+        }
+
+        if (is_null($userID)) {
+            $sql .= " WHERE memberShipID = :memberShipID";
+        }
+
+        $stmt = $conn->prepare($sql);
+
+        if (!is_null($userID)) {
+            $stmt->bindParam(':userID', $userID, PDO::PARAM_STR);
+        }
+        if (is_null($userID)) {
+            $stmt->bindParam(':memberShipID', $this->getMemberShipID(), PDO::PARAM_STR);
+        }
+
+        try {
+            $stmt->execute();
+            return true;
+        } catch (PDOException $e) {
+            return "Delete membership entry failed:" . $e->getMessage();
+        }
+    }
+
+    public function doesExist($conn)
+    {
+        $sql = "SELECT memberShipID FROM membershipID WHERE memberShipID = :memberShipID LIMIT 1";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':memberShipID', $this->getMemberShipID(), PDO::PARAM_STR);
+        try {
+            $stmt->execute();
+            $results = $stmt->fetchAll();
+            if (count($results) > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (PDOException $e) {
+            return "Check membership record exists query failed: " . $e->getMessage();
+        }
+    }
+
+    //List memberships - ALL or by userID
+
+    public function listAllMemberships($conn, $userID = null)
+    {
+        $sql = "SELECT * FROM memberships";
+        if (!is_null($userID)) {
+            $sql .= " WHERE userID = :userID";
+        }
+        $stmt = $conn->prepare($sql);
+
+        if (!is_null($userID)) {
+            $stmt->bindParam(':userID', $userID, PDO::PARAM_STR);
+        }
+
+        try {
+            $stmt->execute();
+            $results = $stmt->fetchAll();
+            return $results;
+        } catch (PDOException $e) {
+            return "List all memberships results query failed: " . $e->getMessage();
+        }
+    }
+
+    //Boolean checkers
+
+    public function isPaid($conn)
+    {
+        $sql = "SELECT memberShipID FROM memberships ms WHERE ms.paid = 1  AND ms.memberShipID = :memberShipID";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':memberShipID', $this->getMemberShipID(), PDO::PARAM_STR);
+
+        try {
+            $stmt->execute();
+            $results = $stmt->fetchAll();
+
+            if ($stmt->rowCount() > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (PDOException $e) {
+            return "Is user paid membership query failed: " . $e->getMessage();
+        }
+    }
+
+
+//Display type of membership based on type flag 0 = one semester, 1 = full membership
+    public function displayType()
+    {
+        switch ($this->getType()) {
+            case 0:
+                return "One Semester";
+                break;
+
+            case 1:
+                return "Full Membership";
+                break;
+        }
+    }
+
+
+    //Input validation
+
+    public function isInputValid($startDate, $endDate)
+    {
+        if ((strtotime($endDate) > strtotime($startDate))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 
 }
