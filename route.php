51f274f2f526d799ae3062d9125d119cfcec39ec
Taity-mini <1504693@rgu.ac.uk>
2017-03-20 00:04:26 +0000

    Router Updated *Added most of the URL parameters to rule array *Added some of the call functions to switch case

--- route.php
+++ route.php
@@ -39,21 +39,76 @@ $rules = array(
 //
 //main pages
 //
-    'about' => "/about",
-    'contactus' => "/contactus",
-    'blog' => "/blog",
-    'blog_article' => "/blog/(?'blogID'[\w\-]+)",
-//
+
+//    'contactus' => "/contactus",
+//    'blog' => "/blog",
+//    'blog_article' => "/blog/(?'blogID'[\w\-]+)",
+
+//About us
+    'about' => "/about/club_information",
+    'committee' => "/about/committee",
+    'join_us' => "/about/join_us",
+
+//Events
+    'events' => "/events/",
+    'event_view' => "/events/view/(?'compID'[\w\-]+)",
+    'trip_events' => "/events/trips",
+    'comp_events' => "/events/competitions",
+
+    //Competitions
+    'competitions' => "/competitions/",
+    'competition_results' => "/competitions/result/(?'compID'[\w\-]+)",
+    'competition_edit' => "/competitions/edit/(?'compID'[\w\-]+)",
+    'competition_add' => "/competitions/add",
+
+//Galleries
+    'gallery' => "/gallery",
+    'gallery_events' => "/gallery/events",
+    'gallery_add' => "/gallery/add",
+    'gallery_upload' => "/gallery/upload",
+    'gallery_photo' => "/gallery/photo/(?'photoID'[\w\-]+)",
+    'gallery_photo_edit' => "/gallery/photo/edit/(?'photoID'[\w\-]+)",
+    'gallery_view' => "/gallery/album/(?'albumID'[\w\-]+)",
+    'gallery_edit' => "/gallery/edit/(?'albumID'[\w\-]+)",
+    'personal_album' => "/gallery/album/personal",
+
+//Profile
+    'profile' => "/profile/",
+    'profile_view' => "/profile/view/(?'userID'[\w\-]+)",
+
+
+//Climbing Log
+    'climbing_log' => "/climbing_log",
+    //Logbook
+
+    'climbing_log_logbook' => "/climbing_log/logbook/(?'logID'[\w\-]+)",
+    //Locations
+    'climbing_log_locations' => "/climbing_log/locations",
+    'climbing_log_location_edit' => "/climbing_log/locations/(?'locationID'[\w\-]+)",
+
+    //Routes
+    'climbing_log_route' => "/climbing_log/route/(?'routeID'[\w\-]+)",
+    'climbing_log_route_edit' => "/climbing_log/route/edit/(?'routeID'[\w\-]+)",
+
+//Committee (Admin Pages)
+    'member_management' => "/committee/member_management",
+    'event_management' => "/committee/event_management",
+    'agenda' => "/committee/agenda",
+
+
 //News page
     'news' => "/news",
+    'news_add' => "/news/add",
     'news_article' => "/news/(?'newsID'[\w\-]+)",
+    'news_edit' => "/news/edit/(?'newsID'[\w\-]+)",
 
+//Search
+    'search' => "/search",
 
-//Admin Pages
-//
+//Login and register Pages
     'login' => "/login",
-    'create_article' => "/createarticle",
     'logout' => "/logout",
+    'register' => "/register",
 //
 // Home Page
 //
@@ -68,15 +123,81 @@ $uri = urldecode($uri);
 foreach ($rules as $action => $rule) {
     if (preg_match('~^' . $rule . '$~i', $uri, $params)) {
 
-
         switch ($action) {
             case 'home':
                 call('pages', $action);
                 break;
 
+            //News
             case 'news':
                 call('news', 'index');
                 break;
+
+            case 'news_add':
+                call('news', 'addNews');
+                break;
+
+            case 'news_edit':
+                call('news', 'editNews');
+                break;
+
+            case 'news_article':
+                call('news', 'article');
+                break;
+
+
+            //About
+            case 'about':
+                call('about', 'club');
+                break;
+
+            case 'committee':
+                call('about', $action);
+                break;
+
+            case 'join_us':
+                call('about', 'joinUs');
+                break;
+
+            // Events
+
+            //Gallery
+            case 'gallery':
+                call('gallery', 'home');
+                break;
+
+            case 'gallery_events':
+                call('gallery', 'events');
+                break;
+
+            case 'gallery_photo':
+                call('gallery', 'viewPhoto');
+                break;
+
+            case 'gallery_add':
+                call('gallery', 'addAlbum');
+                break;
+
+            case 'gallery_upload':
+                call('gallery', 'addPhoto');
+                break;
+
+            case 'gallery_view':
+                call('gallery', 'viewAlbum');
+                break;
+
+            case 'gallery_edit':
+                call('gallery', 'editAlbum');
+                break;
+
+            case 'gallery_photo_edit':
+                call('gallery', 'editPhoto');
+                break;
+
+            case 'personal_album':
+                call('gallery', 'personalAlbum');
+                break;
+
 
             default:
                 call('pages', 'error');
