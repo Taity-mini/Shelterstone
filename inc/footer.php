c3b42b5cdd1c3baf814f9462d33ed5b5591e8127
Taity-mini <1504693@rgu.ac.uk>
2017-03-24 15:19:46 +0000

    Updated Header/footer *Split up design template between header/footer *Used $domain variable for absolute links *Added all links from router to navigation

--- inc/footer.php
+++ inc/footer.php
@@ -4,4 +4,32 @@
  * User: Andrew Tait (1504693)
  * Date: 04/03/2017
  * Time: 18:00
+ * Footer include file used in the design layout
  */
+?>
+<!--END Main Content-->
+</div>
+<!--START FOOTER-->
+<footer class="row">
+        <hr/>
+        <div class="row">
+            <div class="large-6 columns">
+
+                <p>&copy; <?php echo date("Y"); ?> Copyright Shelterstone</p>
+            </div>
+            <div class="large-6 columns">
+
+                <ul class="inline-list right">
+                    <li><a href="#">Links go here</a></li>
+                </ul>
+            </div>
+        </div>
+</footer>
+<!--END FOOTER-->
+<script src="<?php echo $domain ?>/js/vendor/jquery.js"></script>
+<script src="<?php echo $domain ?>/js/vendor/foundation.js"></script>
+<script>
+    $(document).foundation();
+</script>
+</body>
+</html>
--- inc/header.php
+++ inc/header.php
@@ -4,4 +4,104 @@
  * User: Andrew Tait (1504693)
  * Date: 04/03/2017
  * Time: 17:59
+ * Header include file used in the design layout
  */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>RGU ShelterStone</title>
+    <link rel="stylesheet" href="<?php echo $domain ?>/css/foundation.min.css">
+    <link rel="stylesheet" href="<?php echo $domain ?>/css/app.css">
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css" rel="stylesheet">
+    <!--Favicon code START-->
+
+    <!--Favicon code END -->
+
+</head>
+<body>
+<div class="row column text-center">
+    <a href="<?php echo $domain ?>/"><img src="<?php echo $domain ?>/img/ShelterstoneLogo_Small.svg"/></a>
+</div>
+
+
+<!-- Start Top Bar -->
+<div class="title-bar" data-responsive-toggle="main-menu" data-hide-for="large">
+    <button class="menu-icon" type="button" data-toggle></button>
+    <div class="title-bar-title">Menu</div>
+</div>
+
+<!-- Start Top Bar -->
+<div class="top-bar" id="main-menu">
+    <div class="top-bar-left">
+        <ul class="menu" data-responsive-menu="drilldown large-dropdown">
+            <li class="menu-text">RGU: Shelterstone</li>
+            <li class="has-submenu">
+                <a href="<?php echo $domain ?>/">Home</a>
+                <ul class="submenu menu vertical" data-submenu>
+                    <li><a href="<?php echo $domain ?>/news">News</a></li>
+                    <li><a href="<?php echo $domain ?>/news/announcements">Announcements</a></li>
+                </ul>
+            </li>
+            <li class="has-submenu">
+                <a href="#">About</a>
+                <ul class="submenu menu vertical" data-submenu>
+                    <li><a href="<?php echo $domain ?>/about/club_information">Club Information</a></li>
+                    <li><a href="<?php echo $domain ?>/about/committee">Committee</a></li>
+                    <li><a href="<?php echo $domain ?>/about/join_us">Join Us!</a></li>
+                    <li><a href="<?php echo $domain ?>/about/history">History</a></li>
+                </ul>
+            </li>
+            <li class="has-submenu">
+                <a href="<?php echo $domain ?>/events">Events</a>
+                <ul class="submenu menu vertical" data-submenu>
+                    <li><a href="<?php echo $domain ?>/events">Event List</a></li>
+                    <li><a href="<?php echo $domain ?>/events/trips">Trips</a></li>
+                    <li><a href="<?php echo $domain ?>/events/competitions">Competitions</a></li>
+                </ul>
+            </li>
+            <li class="has-submenu">
+                <a href="<?php echo $domain ?>/gallery">Galleries</a>
+                <ul class="submenu menu vertical" data-submenu>
+                    <li><a href="<?php echo $domain ?>/gallery/events">Events</a></li>
+                    <li><a href="<?php echo $domain ?>/gallery/album/personal">Personal Album</a></li>
+                </ul>
+            </li>
+            <li class="has-submenu">
+                <a href="<?php echo $domain ?>/profile">Profile</a>
+                <ul class="submenu menu vertical" data-submenu>
+                    <li><a href="<?php echo $domain ?>/climbing_log">Climbing Log</a></li>
+                    <li><a href="<?php echo $domain ?>/profile">Personal Details</a></li>
+                </ul>
+            </li>
+            <li class="has-submenu">
+                <a href="#">Committee</a>
+                <ul class="submenu menu vertical" data-submenu>
+                    <li><a href="<?php echo $domain ?>/committee/member_management">Member Management</a></li>
+                    <li><a href="<?php echo $domain ?>/committee/event_management">Event Management</a></li>
+                    <li><a href="<?php echo $domain ?>/committee/agenda">Agenda's & Minutes</a></li>
+                </ul>
+            </li>
+            <li class="has-submenu">
+                <a href="<?php echo $domain ?>/login">Login</a>
+                <ul class="submenu menu vertical" data-submenu>
+                    <li><a href="<?php echo $domain ?>/login">Sign in</a></li>
+                    <li><a href="<?php echo $domain ?>/register">Register</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+    <div class="top-bar-right text-center">
+        <ul class="menu">
+            <li><input width="100%" type="search" placeholder="Search">
+            <li>
+                <button type="button" class="button">Search</button>
+            </li>
+        </ul>
+    </div>
+</div>
+<!-- End Top Bar -->
+<!-- Main Content-->
+<div class="row" id="content">
