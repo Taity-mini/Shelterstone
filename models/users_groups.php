1a4c58751db635004f03629e7b662f03b9a4ec02
Taity-mini <1504693@rgu.ac.uk>
2017-04-03 17:08:07 +0100

    User group checks updated *Removed all user_group references *Added role based queries *Security checks for user access in place

--- models/users_groups.php
+++ models/users_groups.php
@@ -1,13 +1,12 @@
 <?php
+
 /**
  * Created by PhpStorm.
- * User: Andrew
+ * User: Andrew Tait (1504693)
  * Date: 04/03/2017
  * Time: 19:28
  * User groups class
  */
-
-
 class users_groups
 {
 
@@ -26,7 +25,7 @@ class users_groups
 
     public function getGroupID()
     {
-      return $this->groupID;
+        return $this->groupID;
     }
 
     public function getGroupName()
@@ -73,7 +72,7 @@ class users_groups
     public function create($conn)
     {
         $sql = "INSERT INTO user_groups VALUES(:userID, :groupID)";
-        $stmt = $conn ->prepare($sql);
+        $stmt = $conn->prepare($sql);
 
         $stmt->bindParam(':userID', $this->getUserID(), PDO::PARAM_STR);
         $stmt->bindParam(':groupID', $this->getGroupID(), PDO::PARAM_STR);
@@ -136,7 +135,8 @@ class users_groups
     }
 
     //List all groups for drop down menus
-    public function listAllGroups($conn) {
+    public function listAllGroups($conn)
+    {
         $sql = "SELECT groupID, groupName FROM groups";
         $stmt = $conn->prepare($sql);
 
@@ -145,7 +145,7 @@ class users_groups
             $results = $stmt->fetchAll();
 
             $array = array();
-            foreach($results as $row) {
+            foreach ($results as $row) {
                 $array[$row['groupID']] = $row['groupName'];
             }
             return $array;
@@ -154,12 +154,12 @@ class users_groups
         }
     }
 
-    //Check permissions
+    //Group/security permissions
 
     //Admin ID 1
     public function isAdministrator($conn, $userID)
     {
-        $sql = "SELECT u.userID FROM user u, groups g WHERE u.groupID = g.groupID AND u.groupID = 1 AND u.userID = :userID";
+        $sql = "SELECT u.userID FROM users u, groups g WHERE u.groupID = g.groupID AND u.groupID = 1 AND u.userID = :userID";
 
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':userID', $userID, PDO::PARAM_STR);
@@ -178,10 +178,140 @@ class users_groups
         }
     }
 
-    //Current President ID 2
+
+    //Is user apart of the committee ID:2
+
+    public function isUserCommittee($conn, $userID)
+    {
+        $sql = "SELECT u.userID FROM users u, groups g WHERE u.groupID = g.groupID AND u.groupID = 2 AND u.userID = :userID";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':userID', $userID, PDO::PARAM_STR);
+
+        try {
+            $stmt->execute();
+            $results = $stmt->fetchAll();
+
+            if ($stmt->rowCount() > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (PDOException $e) {
+            return "Query failed: " . $e->getMessage();
+        }
+    }
+
+
+    //Standard users - Member ID: 3
+
+    public function isUserMember($conn, $userID)
+    {
+        $sql = "SELECT u.userID FROM users u, groups g WHERE u.groupID = g.groupID AND u.groupID = 3 AND u.userID = :userID";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':userID', $userID, PDO::PARAM_STR);
+
+        try {
+            $stmt->execute();
+            $results = $stmt->fetchAll();
+
+            if ($stmt->rowCount() > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (PDOException $e) {
+            return "Query failed: " . $e->getMessage();
+        }
+    }
+
+
+    //Past Members - ID :4
+
+    public function isUserPastMember($conn, $userID)
+    {
+        $sql = "SELECT u.userID FROM users u, groups g WHERE u.groupID = g.groupID AND u.groupID = 3 AND u.userID = :userID";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':userID', $userID, PDO::PARAM_STR);
+
+        try {
+            $stmt->execute();
+            $results = $stmt->fetchAll();
+
+            if ($stmt->rowCount() > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (PDOException $e) {
+            return "Query failed: " . $e->getMessage();
+        }
+    }
+
+
+    //Security checks functions
+
+    //User pages
+    public function userFullAccess($conn, $userID)
+    {
+        if ($this->isAdministrator($conn, $userID) || $this->isUserCommittee($conn, $userID)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function userViewAccess($conn, $userID)
+    {
+        if ($this->userFullAccess($conn, $userID)) {
+            return true;
+        } else if ($this->isUserMember($conn, $userID)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    //List Committee members
+
+    public function ListCommittee($conn)
+    {
+        //$sql = "SELECT userID FROM users u, user_groups ug, groups g WHERE ug.groupID = g.groupID AND g.groupID IN (2,3,4,5,6) AND u.userID = :userID";
+
+        $sql = "SELECT u.userID FROM users u, groups g WHERE u.groupID = g.groupID AND u.groupID = 2 AND u.userID = :userID ORDER BY g.groupID ASC";
+
+        $stmt = $conn->prepare($sql);
+
+        try {
+            $stmt->execute();
+            $results = $stmt->fetchAll();
+
+            $array = array();
+            foreach ($results as $result) {
+                if (array_key_exists($result["groupID"], $array)) {
+                    $list = $array[$result["groupID"]];
+                    array_push($list, $result["userID"]);
+                    $array[$result["groupID"]] = $list;
+                } else {
+                    $list = array();
+                    array_push($list, $result["userID"]);
+                    $array[$result["groupID"]] = $list;
+                }
+            }
+            return $array;
+        } catch (PDOException $e) {
+            return "List Committee Query failed: " . $e->getMessage();
+        }
+    }
+
+    //Committee Roles
+
+    //Current President - based on role
     public function isUserPresident($conn, $userID)
     {
-        $sql = "SELECT ug.userID FROM user_groups ug, groups g WHERE ug.groupID = g.groupID AND ug.groupID = 2 AND ug.userID = :userID";
+        $sql = "SELECT u.userID, u.role FROM users u, groups g WHERE u.groupID = g.groupID AND u.groupID = 2 AND u.role='President' AND u.userID = :userID";
 
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':userID', $userID, PDO::PARAM_STR);
@@ -203,7 +333,7 @@ class users_groups
     //Current competitions vice president: ID 3
     public function isUserCompsPres($conn, $userID)
     {
-        $sql = "SELECT ug.userID FROM user_groups ug, groups g WHERE ug.groupID = g.groupID AND ug.groupID = 3 AND ug.userID = :userID";
+        $sql = "SELECT u.userID, u.role FROM users u, groups g WHERE u.groupID = g.groupID AND u.groupID = 2 AND u.role='Competitions Vice president' AND u.userID = :userID";
 
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':userID', $userID, PDO::PARAM_STR);
@@ -225,7 +355,7 @@ class users_groups
     //Current outdoor events/trips vice president ID: 4
     public function isUserTripsPres($conn, $userID)
     {
-        $sql = "SELECT ug.userID FROM user_groups ug, groups g WHERE ug.groupID = g.groupID AND ug.groupID = 4 AND ug.userID = :userID";
+        $sql = "SELECT u.userID, u.role FROM users u, groups g WHERE u.groupID = g.groupID AND u.groupID = 2 AND u.role='Outdoor Trips Chair' AND u.userID = :userID";
 
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':userID', $userID, PDO::PARAM_STR);
@@ -244,10 +374,10 @@ class users_groups
         }
     }
 
-    //Current social secretary ID: 5
-    public function isUserSocialSecretary($conn, $userID)
+    //Current Social Chair
+    public function isUserSocialChair($conn, $userID)
     {
-        $sql = "SELECT ug.userID FROM user_groups ug, groups g WHERE ug.groupID = g.groupID AND ug.groupID = 5 AND ug.userID = :userID";
+        $sql = "SELECT u.userID, u.role FROM users u, groups g WHERE u.groupID = g.groupID AND u.groupID = 2 AND u.role='Social Chair' AND u.userID = :userID";
 
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':userID', $userID, PDO::PARAM_STR);
@@ -266,11 +396,10 @@ class users_groups
         }
     }
 
-    //Current health coordinator ID: 6
-
-    public function isUserHealthCoordinator($conn, $userID)
+    //Current Fundraising Chair
+    public function isUserFundraisingChair($conn, $userID)
     {
-        $sql = "SELECT ug.userID FROM user_groups ug, groups g WHERE ug.groupID = g.groupID AND ug.groupID = 6 AND ug.userID = :userID";
+        $sql = "SELECT u.userID, u.role FROM users u, groups g WHERE u.groupID = g.groupID AND u.groupID = 2 AND u.role='Fundraising Chair' AND u.userID = :userID";
 
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':userID', $userID, PDO::PARAM_STR);
@@ -289,11 +418,11 @@ class users_groups
         }
     }
 
-    //Standard users - Member ID: 7
+    //Current safety officer ID: 6
 
-    public function isUserMember($conn, $userID)
+    public function isUserSafetyOfficer($conn, $userID)
     {
-        $sql = "SELECT ug.userID FROM user_groups ug, groups g WHERE ug.groupID = g.groupID AND ug.groupID = 7 AND ug.userID = :userID";
+        $sql = "SELECT u.userID, u.role FROM users u, groups g WHERE u.groupID = g.groupID AND u.groupID = 2 AND u.role='Safety Officer' AND u.userID = :userID";
 
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':userID', $userID, PDO::PARAM_STR);
@@ -312,83 +441,6 @@ class users_groups
         }
     }
 
-
-    //Past Members - ID :8
-
-    public function isUserPastMember($conn, $userID)
-    {
-        $sql = "SELECT ug.userID FROM user_groups ug, groups g WHERE ug.groupID = g.groupID AND ug.groupID = 8 AND ug.userID = :userID";
-
-        $stmt = $conn->prepare($sql);
-        $stmt->bindParam(':userID', $userID, PDO::PARAM_STR);
-
-        try {
-            $stmt->execute();
-            $results = $stmt->fetchAll();
-
-            if ($stmt->rowCount() > 0) {
-                return true;
-            } else {
-                return false;
-            }
-        } catch (PDOException $e) {
-            return "Query failed: " . $e->getMessage();
-        }
-    }
-
-    //Is user apart of the committee
-
-    public function isUserCommittee($conn, $userID)
-    {
-        $sql = "SELECT userID FROM users u, user_groups ug, groups g WHERE ug.groupID = g.groupID AND g.groupID IN (2,3,4,5,6,) AND u.userID = :userID";
-        $stmt = $conn->prepare($sql);
-        $stmt->bindParam(':userID', $userID, PDO::PARAM_STR);
-
-        try {
-            $stmt->execute();
-            $results = $stmt->fetchAll();
-
-            if ($stmt->rowCount() > 0) {
-                return true;
-            } else {
-                return false;
-            }
-        } catch (PDOException $e) {
-            return "Query failed: " . $e->getMessage();
-        }
-    }
-
-    //List Committee members
-
-    public function ListCommittee($conn)
-    {
-        //$sql = "SELECT userID FROM users u, user_groups ug, groups g WHERE ug.groupID = g.groupID AND g.groupID IN (2,3,4,5,6) AND u.userID = :userID";
-
-        $sql = "SELECT u.userID, g.groupID FROM users u, user_groups ug, groups g WHERE ug.groupID = g.groupID AND g.groupID IN (2,3,4,5,6) AND u.userID = :userID ORDER BY g.groupIDASC";
-
-        $stmt = $conn->prepare($sql);
-
-        try {
-            $stmt->execute();
-            $results = $stmt->fetchAll();
-
-            $array = array();
-            foreach ($results as $result) {
-                if (array_key_exists($result["groupID"],$array)) {
-                    $list = $array[$result["groupID"]];
-                    array_push($list,$result["userID"]);
-                    $array[$result["groupID"]] = $list;
-                } else {
-                    $list = array();
-                    array_push($list,$result["userID"]);
-                    $array[$result["groupID"]] = $list;
-                }
-            }
-            return $array;
-        } catch (PDOException $e) {
-            return "List Committee Query failed: " . $e->getMessage();
-        }
-    }
 
     //Validation functions
     public function isInputValid($name, $description)
